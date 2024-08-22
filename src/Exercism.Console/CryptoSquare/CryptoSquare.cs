684d90e5dc0650e0035abe06973a56b68bd1b253
Jairo Bionez <jairo@treeinova.com.br>
2024-08-21 23:14:51 -0300

    exercise:cryptosquare

---
+++ src/Exercism.Console/CryptoSquare/CryptoSquare.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+
+namespace Exercism.Console.CryptoSquare
+{
+    public static class CryptoSquare
+    {
+        public static string NormalizedPlaintext(string plaintext)
+        {
+            if (string.IsNullOrEmpty(plaintext))
+                return string.Empty;
+
+            // Passo 1: Remover acentos
+            string normalizedString = plaintext.Normalize(NormalizationForm.FormD);
+            string stringWithoutAccents = new string(normalizedString
+                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                .ToArray());
+
+            // Passo 2: Remover caracteres especiais e símbolos
+            string result = Regex.Replace(stringWithoutAccents, @"[^a-zA-Z0-9\s]", "");
+
+            return result.Trim().Replace(" ", string.Empty).ToLower();
+        }
+
+        public static string PlaintextSegments(string plaintext)
+        {
+            if (string.IsNullOrEmpty(plaintext))
+                return string.Empty;
+
+            var columns = 0;
+            var rows = 0;
+            var wordSize = plaintext.Length;
+            var sqrtCount = plaintext.Length;
+
+            while (rows == 0)
+            {
+                var sqrt = Math.Sqrt(sqrtCount);
+                if ((sqrt % 1 == 0 || sqrt % 2 == 0))
+                {
+                    if (sqrtCount - wordSize == 1 || sqrtCount == wordSize)
+                        rows = (int)sqrt;
+                    else if (sqrtCount > wordSize)
+                        rows = (int)sqrt - 1;
+                    else
+                        rows = (int)sqrt + 1;
+
+                    columns = (int)sqrt;
+                }
+
+                sqrtCount++;
+            }
+
+            var squareArray = new string[rows, columns];
+            var codifiedArray = new string[columns];
+
+            var stringCount = 0;
+
+            for (var r = 0; r < rows; r++)
+            {
+                for (var c = 0; c < columns; c++)
+                {
+                    squareArray[r, c] = stringCount < wordSize ? plaintext[stringCount].ToString() : " ";
+                    stringCount++;
+                }
+            }
+
+            for (var c = 0; c < columns; c++)
+            {
+                for (var r = 0; r < rows; r++)
+                {
+                    codifiedArray[c] += squareArray[r, c];
+                }
+            }
+
+
+            return string.Join(' ', codifiedArray);
+        }
+
+        public static string Encoded(string plaintext)
+        {
+            if (string.IsNullOrEmpty(plaintext))
+                return string.Empty;
+
+            var normalizedText = NormalizedPlaintext(plaintext);
+            var plaintextSegments = PlaintextSegments(normalizedText);
+
+
+            return plaintextSegments;
+        }
+
+        public static string Ciphertext(string plaintext)
+        {
+            if (string.IsNullOrEmpty(plaintext))
+                return string.Empty;
+
+            var encodedMessage = Encoded(plaintext);
+            return encodedMessage;
+        }
+    }
+}
---
+++ src/Exercism.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Exercism.Console.CryptoSquare;
+
+namespace Exercism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://exercism.org/tracks/csharp/exercises/crypto-square
+            //var cryptoSquare = CryptoSquare.Ciphertext("This is fun!");
+            //var cryptoSquare2 = CryptoSquare.Ciphertext("If man was meant to stay on the ground, god would have given us roots.");
+            //var cryptoSquare3 = CryptoSquare.Ciphertext("Chill out.");
+        }
+    }
+}
